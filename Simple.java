1f87278071095fe16189ac18a6a5c215d20921ce
Nikki Lin <nikkiylin@gmail.com>
2019-10-14 21:05:26 -0700

    added a thanks note

--- Simple.java
+++ Simple.java
@@ -1,5 +1,7 @@
+// Simple class
 public class Simple {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    System.out.println("Thanks for helping me!");
   }
 }
