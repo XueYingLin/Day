21362d4cda1af0a41e9456c651d9e19d5dcd30b2
Nikki Lin <nikkiylin@gmail.com>
2019-10-14 21:15:14 -0700

    deleted the Simple.java

--- Simple.java
+++
@@ -1,7 +0,0 @@
-// Simple class
-public class Simple {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-    System.out.println("Thanks for helping me!");
-  }
-}
